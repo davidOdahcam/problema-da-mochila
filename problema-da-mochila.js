10689f37b23e0d88a827ed1e96490ad95ace38cc
David dos Santos Machado <david.s.machado99@gmail.com>
2023-07-07 11:49:23 -0300

    Definindo contexto e os itens que serão aplicados

---
+++ problema-da-mochila.js
@@ -0,0 +1,7 @@
+const itens = [
+  { nome: "Ativo 1", peso: 6,  valor: 8 },
+  { nome: "Ativo 2", peso: 10, valor: 15 },
+  { nome: "Ativo 3", peso: 3,  valor: 5 },
+  { nome: "Ativo 4", peso: 1,  valor: 3 },
+  { nome: "Ativo 5", peso: 7,  valor: 12 },
+];
