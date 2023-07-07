bccb8a78a5594b6d16aafa5ad2dea11a82cde47d
David dos Santos Machado <david.s.machado99@gmail.com>
2023-07-07 11:53:29 -0300

    Gerando população

--- problema-da-mochila.js
+++ problema-da-mochila.js
@@ -1,3 +1,5 @@
+const tamanhoPopulacao = 10;
+
 const itens = [
   { nome: "Ativo 1", peso: 6,  valor: 8 },
   { nome: "Ativo 2", peso: 10, valor: 15 },
@@ -5,3 +7,13 @@ const itens = [
   { nome: "Ativo 4", peso: 1,  valor: 3 },
   { nome: "Ativo 5", peso: 7,  valor: 12 },
 ];
+
+const executarAlgoritmoGenetico = () => {
+    let populacao = Array.from({ length: tamanhoPopulacao }, () =>
+      Array.from({ length: itens.length }, () => (Math.random() < 0.5 ? 0 : 1))
+    );
+
+    console.log(populacao);
+};
+
+executarAlgoritmoGenetico();
